df90bfcd8511b2ffe1c102e40bcfbee091283086
zainab-nooh <zain3b.nooh@gmail.com>
2025-08-05 14:55:21 +0300

    Fixi login routes

--- controllers/businessOwners/routeController.js
+++ controllers/businessOwners/routeController.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const dataController  = require('./dataController')
 const viewController = require('./viewController')
 const businessViewController  = require('../business/viewController')
+const businessDataController = require('../business/dataController')
 //More consts to be added
 
 
@@ -11,7 +12,7 @@ router.get('/', viewController.signUp)//Show SignUp form
 
 
 // router.post('/login', dataController.loginBusinessOwner, 'more stuff!! --> need to redirect to page Business Profile page') // Log in Business Owner
-router.post('/login', dataController.loginBusinessOwner, businessViewController.index) // Log in Business Owner
+router.post('/login', dataController.loginBusinessOwner, businessDataController.index, businessViewController.index) // Log in Business Owner
 router.get('/login', viewController.signIn) // show up Log in page 
 
 
